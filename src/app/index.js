ec21c1fa22dcdf3a8c8cfa9d63ef7d608221dca9
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2023-12-10 19:00:47 +0100

    git commit

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,7 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
+import { refs } from './refs';
 import { createMarkup } from './createMarkup';
 import { PixabayAPI } from './PixabayAPI';
 import { notifyInit } from './notifyInit';
